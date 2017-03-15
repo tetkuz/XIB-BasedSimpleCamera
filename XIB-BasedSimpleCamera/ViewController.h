e01644d27457cf69333a02a282efe75c2e0d0c76
Shota TAMURA <tamura.shota@kiroru-inc.jp>
2017-03-15 10:58:40 +0900

    Make a Camera App

--- XIB-BasedSimpleCamera/ViewController.h
+++ XIB-BasedSimpleCamera/ViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
+- (IBAction)shootPhoto:(UIButton *)sender;
 
 @end
 
--- XIB-BasedSimpleCamera/ViewController.m
+++ XIB-BasedSimpleCamera/ViewController.m
@@ -25,5 +25,22 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)shootPhoto:(UIButton *)sender {
+    if ([UIImagePickerController availableCaptureModesForCameraDevice:UIImagePickerControllerCameraDeviceFront] != nil) {
+        UIImagePickerController *picker = [UIImagePickerController new];
+        picker.allowsEditing = false;
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        picker.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
+        picker.showsCameraControls = true;
+        
+        picker.modalPresentationStyle = UIModalPresentationFullScreen;
+        [self presentViewController:picker animated:YES completion:nil];
+    } else {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"No Camera" message:@"Sorry, this device has no camera" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+        [alert addAction:action];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+}
 
 @end
